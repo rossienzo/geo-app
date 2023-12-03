0c93d2fb449d9927ef7c567a75e440f17b5baf9a
Enzo Rossi <enzorossi97@hotmail.com>
2023-12-02 21:11:50 -0300

    fix: corrigindo problema no websocket

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,68 @@
 import express from "express";
 import routes from "./routes";
 import http from "http";
+import * as WebSocket from "ws";
+import { MQTTService } from "./services/mqtt.service";
+import cors from "cors";
+
+declare module "express-serve-static-core" {
+    interface Request {
+      wss: WebSocket.Server;
+      mqttService: MQTTService; // Substitua 'MQTTService' pelo tipo real do seu serviço MQTT
+    }
+}
 
 const app = express();
 const server = http.createServer(app);
-  
+const wss = new WebSocket.Server({ server });
+const mqttService = new MQTTService(wss);
+
 // configura o ejs	
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(`${__dirname}/public`));
-
+app.use(cors());
 app.use(express.json());
+
+
+// Array para armazenar clientes WebSocket conectados
+const clients: WebSocket[] = [];
+
+// Configuração do servidor WebSocket
+wss.on("connection", (ws: WebSocket) => {
+	console.log("Cliente WebSocket conectado");
+    
+	clients.push(ws);
+
+	ws.on("message", (message) => {
+		console.log(`Mensagem recebida do cliente WebSocket: ${message}`);
+
+		// Envia a mensagem para todos os clientes conectados
+		clients.forEach((client) => {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(message);
+			}
+		});
+	});
+  
+	ws.on("close", () => {
+		// Remove o cliente desconectado do array
+		console.log("Cliente WebSocket desconectado");
+		
+		const index = clients.indexOf(ws);
+		if (index !== -1) {
+			clients.splice(index, 1);
+		}
+	});
+});
+
+// Middleware Websocket e MQTT
+app.use((req, res, next) => {
+	req.wss = wss;
+	req.mqttService = mqttService;
+	next();
+});
+
 app.use(routes);
 
 server.listen(3000, () => console.log(`Server running in http://localhost:${3000}`));
